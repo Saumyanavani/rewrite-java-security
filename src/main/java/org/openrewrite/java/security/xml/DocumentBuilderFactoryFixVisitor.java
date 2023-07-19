d31a57257c67d352d640abc9d9d77d8df441f212
Saumyanavani <navanisaumya@gmail.com>
2023-07-19 11:06:50 -0400

    Started adding support for DocumentBuilderFactory in the XXE Recipe (WIP).

--- src/main/java/org/openrewrite/java/security/xml/DocumentBuilderFactoryFixVisitor.java
+++ src/main/java/org/openrewrite/java/security/xml/DocumentBuilderFactoryFixVisitor.java
@@ -1,2 +1,54 @@
-package org.openrewrite.java.security.xml;public class DocumentBuilderFactoryFixVisitor {
+package org.openrewrite.java.security.xml;
+
+import lombok.AllArgsConstructor;
+import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.TypeUtils;
+
+import javax.xml.stream.XMLInputFactory;
+
+@AllArgsConstructor
+public class DocumentBuilderFactoryFixVisitor<P> extends JavaIsoVisitor<P> {
+
+    static final MethodMatcher DBF_NEW_INSTANCE = new MethodMatcher("javax.xml.parsers.DocumentBuilderFactory newInstance*()");
+    static final MethodMatcher DBF_PARSER_SET_FEATURE = new MethodMatcher("javax.xml.parsers.DocumentBuilderFactory setFeature(java.lang.String, boolean)");
+
+    private final ExternalDTDAccumulator acc;
+    private static final String DBF_FQN = "javax.xml.parsers.DocumentBuilderFactory";
+    private static final String DISALLOW_DOCTYPE_DECLARATIONS = "http://apache.org/xml/features/disallow-doctype-decl";
+    private static final String DBF_INITIALIZATION_METHOD = "dbf-initialization-method";
+
+    @Override
+    public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, P ctx) {
+        J.MethodInvocation m = super.visitMethodInvocation(method, ctx);
+        if (DBF_NEW_INSTANCE.matches(m)) {
+            getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, DBF_INITIALIZATION_METHOD, getCursor().dropParentUntil(J.Block.class::isInstance));
+        } else if (DBF_PARSER_SET_FEATURE.matches(m) && m.getArguments().get(0) instanceof J.Identifier) {
+            J.Identifier id = (J.Identifier) m.getArguments().get(0);
+            if (DISALLOW_DOCTYPE_DECLARATIONS.equals(id.getSimpleName())) {
+                getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, DISALLOW_DOCTYPE_DECLARATIONS, getCursor().dropParentUntil(J.Block.class::isInstance));
+            }
+        }
+//        } else if (DBF_PARSER_SET_FEATURE.matches(m) && m.getArguments().get(0) instanceof J.Literal) {
+//            J.Literal literal = (J.Literal) m.getArguments().get(0);
+//            if (TypeUtils.isString(literal.getType())) {
+//                if (XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES.equals(literal.getValue())) {
+//                    getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, SUPPORTING_EXTERNAL_ENTITIES_PROPERTY_NAME, getCursor().dropParentUntil(J.Block.class::isInstance));
+//                } else if (XMLInputFactory.SUPPORT_DTD.equals(literal.getValue())) {
+//                    checkDTDSupport(m);
+//                }
+//            }
+
+        return m;
+    }
+
+    public static TreeVisitor<?, ExecutionContext> create(ExternalDTDAccumulator acc) {
+        return Preconditions.check(new UsesType<>(DBF_FQN, true), new DocumentBuilderFactoryFixVisitor<>(acc));
+    }
 }
--- src/test/java/org/openrewrite/java/security/xml/DocumentBuilderFactoryXXETest.java
+++ src/test/java/org/openrewrite/java/security/xml/DocumentBuilderFactoryXXETest.java
@@ -1,2 +1,60 @@
-package org.openrewrite.java.security.xml;public class DocumentBuilderFactoryXXETest {
+package org.openrewrite.java.security.xml;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.security.XmlParserXXEVulnerability;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.xml.Assertions.xml;
+
+public class DocumentBuilderFactoryXXETest implements RewriteTest{
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new XmlParserXXEVulnerability());
+    }
+
+    @Test
+    void factoryIsNotVulnerable() {
+        //language=java
+        rewriteRun(
+          java(
+            """
+              import javax.xml.parsers.DocumentBuilderFactory;
+              import javax.xml.parsers.ParserConfigurationException; // catching unsupported features
+              import javax.xml.XMLConstants;
+              
+              class myDBFReader {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                
+                void testSetFeature(){
+                    String FEATURE = null;
+                    try {
+                        // This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all
+                        // XML entity attacks are prevented
+                        // Xerces 2 only - http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+                        FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+                        dbf.setFeature(FEATURE, true);
+                    
+                        //per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks"
+                    
+                        // remaining parser logic
+                    } catch (ParserConfigurationException e) {
+                        // This should catch a failed setFeature feature
+                        // NOTE: Each call to setFeature() should be in its own try/catch otherwise subsequent calls will be skipped.
+                        // This is only important if you're ignoring errors for multi-provider support.
+                        logger.info("ParserConfigurationException was thrown. The feature '" + FEATURE
+                        + "' is not supported by your XML processor.");
+                    }
+                }
+              }
+              """
+          )
+        );
+    }
+
+
 }
